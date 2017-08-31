be8694466b2674db58b87f91ddcecf08d447d3eb
Kyle Madison <kylem1337@gmail.com>
2017-08-31 16:46:36 -0700

    Formatted existing source code, removed unused imports

--- src/main/java/privateprojects/pathindexer/Main.java
+++ src/main/java/privateprojects/pathindexer/Main.java
@@ -3,8 +3,8 @@ package privateprojects.pathindexer;
 import java.io.IOException;
 import java.net.URI;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import privateprojects.pathindexer.http.HttpClientWrapper;
 import privateprojects.pathindexer.pojo.PoeNinjaStats;
 
@@ -17,7 +17,7 @@ public class Main {
     HttpClientWrapper simpleHttp = new HttpClientWrapper();
     String poeNinjaResponse = simpleHttp.peformGet(POE_NINJA_URI);
     ObjectMapper objectMapper = new ObjectMapper();
-    PoeNinjaStats poeNinjaStats = objectMapper.readValue(poeNinjaResponse,PoeNinjaStats.class);
+    PoeNinjaStats poeNinjaStats = objectMapper.readValue(poeNinjaResponse, PoeNinjaStats.class);
     System.out.println("Poe Ninja response length: " + poeNinjaResponse.length());
     System.out.println("Poe Ninja response next change id: " + poeNinjaStats.getNextChangeId());
     String poeResponse = simpleHttp.peformGet(POE_URI);
